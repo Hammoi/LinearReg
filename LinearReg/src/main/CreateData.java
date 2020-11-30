58d64d4f013bc6cd63b7481306d929cf20674a3a
Hammoi <hanyiliu0@gmail.com>
2020-11-29 20:30:03 -0800

    Added ability to choose m and b values.

--- LinearReg/src/main/CreateData.java
+++ LinearReg/src/main/CreateData.java
@@ -3,42 +3,39 @@ package main;
 import java.text.DecimalFormat;
 
 public class CreateData {
+	
+	private static final int variableRange = 10000;
+	private static final int inputRange = 100;
+	
 	public static double[][] genRandData(int size) {
+		
+		DecimalFormat df = new DecimalFormat("#.##");
+		
+		double m = Double.parseDouble(df.format(Math.random()*variableRange));
+		double b = Double.parseDouble(df.format(Math.random()*variableRange)); 
+		
+		return genData(m,b,size);
+	}
+	
+	public static double[][] genData(double m, double b, int size) {
+		
+		System.out.println("Using function " + m + "x + " + b + ".");
 		
 		double[] x = new double[size];
 		double[] y = new double[size];
 		
 		DecimalFormat df = new DecimalFormat("#.##");
 		
-		double m = Double.parseDouble(df.format(Math.random()*100)); //Range from 0 to 100
-		double b = Double.parseDouble(df.format(Math.random()*100)); 
-		
-		System.out.println("Using function " + m + "x + " + b + ".");
-		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
 		System.out.println("\nData values:");
 		
 		for(int i = 0; i < size; i++) {
-			x[i] = Double.parseDouble(df.format(Math.random()*100));
+			x[i] = Double.parseDouble(df.format(Math.random()*inputRange));
 			y[i] = m * x[i] + b;
 			System.out.println(x[i] + " | " + y[i]);
 			
 		}
 		
+		return new double[][] {x,y};
 		
-		try {
-			Thread.sleep(2500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println("\n\n");
-		
-		return new double[][]{x,y};
 	}
 }
--- LinearReg/src/main/LinearReg.java
+++ LinearReg/src/main/LinearReg.java
@@ -1,43 +1,85 @@
 package main;
 
 import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner; 
 
 public class LinearReg {
-	private static final int sampleSize = 20;
-	private static final boolean loosenCriteria = false;
-	
+	private static final boolean useRandomData = false; 				//Generate random data?
+	private static final int sampleSize = 40; 							//Size of sample
+	private static final boolean loosenCriteria = true;					//Loosen criteria if no equation found after every *loosenLap* runs?
+	private static final int loosenLap = 100000; 						//How many runs before criteria is loosened
+	private static final double loosenValue = 2; 						//Value to loosen criteria by
+	private static final double learningRate = 0.0001; 					//Learning rate
+	private static final double initCriteria = 0.0000000000000001; 		//Criteria to begin with
+
 	private static double[] x = new double[sampleSize];
 	private static double[] y = new double[sampleSize];
 
-	private static double m = 0; //theta 1
-	private static double b = 0; //theta 0
+	private static double m = 0; //theta 1								//m value to begin with
+	private static double b = 0; //theta 0								//b value to begin with
 
 	public static void main(String[] args) {
 		//LINEAR REGRESSION
 		//mx+b
-		
-		double[][] data = CreateData.genRandData(sampleSize);
+
+		double[][] data = new double[2][];
+
+		if(useRandomData) {
+			
+			data = CreateData.genRandData(sampleSize);
+			
+		} else {
+
+			double inputM = 0;
+			double inputB = 0;
+			
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter m value:");
+			
+			
+			
+			try {
+				inputM = input.nextDouble();
+			} catch(InputMismatchException e) {
+				System.out.println("Invalid m value.");
+			}
+
+			System.out.println("Enter b value:");
+
+			try {
+				inputB = input.nextDouble();
+			} catch(InputMismatchException e) {
+				System.out.println("Invalid b value.");
+			}
+
+
+			data = CreateData.genData(inputM, inputB, sampleSize);
+			input.close();
+		}
+
+
+
 		x = data[0];
 		y = data[1];
-		
-		
+
+
 		if(!(x.length == y.length)) {
 			System.out.println("Faulty data. Abort!!");
 			return;
 		}
 
-		
 
 
-		double learningRate = 0.0001;
-		double criteria = 0.0000000000000001;
 		int runs = 0;
 		int lap = 0;
 		
+		double criteria = initCriteria;
+
 		System.out.println("Starting linear regression.\n");
-		
+
 		long time = System.currentTimeMillis();
-		
+
 		while(cost() > criteria) {
 			double mDerivative = derivative(learningRate, x, y, m, false);
 			double bDerivative = derivative(learningRate, x, y, b, true);
@@ -46,21 +88,23 @@ public class LinearReg {
 			b -= bDerivative;
 
 			runs++;
-			
-			if(runs > 100000) {
-				criteria = loosenCriteria ? criteria * 2 : criteria;
+
+			if(runs > loosenLap) {
+				criteria = loosenCriteria ? (criteria * loosenValue) : criteria;
 				runs = 0;
 				lap += 1;
 			}
-			
-			
-		
-			
+
+
+
+
 		}
-		System.out.println("Complete\ncost: " + cost() + ", run #" + (100000 * lap + runs) + ", took " + (System.currentTimeMillis() - time) + " milleseconds.");
+		System.out.println("Complete\ncost: " + cost() + ", total runs #" + (100000 * lap + runs) + ", took " + (System.currentTimeMillis() - time) + " milleseconds, criteria of " + criteria + ", criteria was " + (loosenCriteria ? "loosened by value of " + loosenValue  + " every " + loosenLap + " runs.\n": "not loosened.\n") 
+				+ "learning rate: " + learningRate + ", initial criteria: " + initCriteria);
+
 		DecimalFormat df = new DecimalFormat("#.###");
 		System.out.println(df.format(m) + "x + " + df.format(b));
-		
+
 
 	}
 
@@ -70,19 +114,19 @@ public class LinearReg {
 
 	private static double cost() {
 		double d = 0;
-		
+
 		double sum = 0;
 		for(int i = 0; i < x.length; i++) {
-			
+
 			//System.out.println(i + " " + (currentFunction(x[i]) - y[i]));
 			sum += Math.pow(currentFunction(x[i]) - y[i], 2);
 		}
-		
+
 		d = sum / (2 * x.length);
-		
+
 		return d;
 	}
-	
+
 	private static double derivative(double lr, double[] x, double[]y, double m, boolean constant) {
 
 
@@ -90,7 +134,7 @@ public class LinearReg {
 
 			double sum = 0;
 			for(int i = 0; i < x.length; i++) {
-				
+
 				//System.out.println(i + " " + (currentFunction(x[i]) - y[i]));
 				sum += currentFunction(x[i]) - y[i];
 			}
@@ -107,7 +151,7 @@ public class LinearReg {
 			}
 
 			//System.out.println("m sum: " + sum);
-			
+
 			double d = (lr * sum) / x.length;
 			return d;
 
